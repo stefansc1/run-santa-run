08cddd1e4bfb1e62303ff6c4bd41d7c478bbb7c6
PaulScheerRLI <104760879+PaulScheerRLI@users.noreply.github.com>
2024-11-24 14:25:52 +0100

    Update audio.js
    remove audio file input handling

--- santa/static/js/audio.js
+++ santa/static/js/audio.js
@@ -12,49 +12,6 @@ const jumpElement = document.getElementById("jump");
 var track3 = audioContext.createMediaElementSource(jumpElement);
 track3.connect(audioContext.destination);
 
-musicFile.onchange = function() {
-  var files = this.files;
-  var file = URL.createObjectURL(files[0]);
-  musicElement.src = file;
-};
-
-countdownFile.onchange = function() {
-  var files = this.files;
-  var file = URL.createObjectURL(files[0]);
-  countdownElement.src = file;
-};
-
-
-jumpFile.onchange = function() {
-  var files = this.files;
-  var file = URL.createObjectURL(files[0]);
-  jumpElement.src = file;
-};
-// Select our play button
-// const playButton = document.getElementById("audioButton");
-
-//playButton.addEventListener(
-//  "click",
-//  () => {
-//    // Check if context is in suspended state (autoplay policy)
-//    if (audioContext.state === "suspended") {
-//      audioContext.resume();
-//    }
-//
-//    // Play or pause track depending on state
-//    if (playButton.dataset.playing === "false") {
-//      console.log("playig");
-//      musicElement.play();
-//      playButton.dataset.playing = "true";
-//    } else if (playButton.dataset.playing === "true") {
-//      musicElement.pause();
-//      playButton.dataset.playing = "false";
-//    }
-//  },
-//  false,
-//);/
-
-
 musicElement.addEventListener(
   "ended",
   () => {
